e7dc93867abedd22bee7857d3c8d678b161bd7e6
Vitor Casadei <vitor.casadei@gmail.com>
2018-07-16 14:30:35 -0300

    Creating graphite exporter structure

---
+++ exporter/graphite/graphite_test.go
@@ -0,0 +1,76 @@
+// Copyright 2018, OpenCensus Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package graphite
+
+import (
+	"context"
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"os"
+	"testing"
+	"time"
+
+	"go.opencensus.io/internal/testpb"
+	"go.opencensus.io/plugin/ochttp"
+	"go.opencensus.io/stats/view"
+	"go.opencensus.io/trace"
+	"golang.org/x/net/context/ctxhttp"
+)
+
+func TestExport(t *testing.T) {
+	projectID, ok := os.LookupEnv("GRAPHITE_TEST_PROJECT_ID")
+	if !ok {
+		t.Skip("GRAPHITE_TEST_PROJECT_ID not set")
+	}
+
+	var exportErrors []error
+
+	exporter, err := NewExporter(Options{ProjectNamespace: projectID })
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer exporter.Flush()
+
+	view.RegisterExporter(exporter)
+	defer view.UnregisterExporter(exporter)
+	
+	server := httptest.NewServer(&ochttp.Handler{Handler: handler})
+	defer server.Close()
+
+	ctx := context.Background()
+	client := &http.Client{
+		Transport: &ochttp.Transport{},
+	}
+	resp, err := ctxhttp.Get(ctx, client, server.URL+"/test/123?abc=xyz")
+	if err != nil {
+		t.Fatal(err)
+	}
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if want, got := "Hello, world!", string(body); want != got {
+		t.Fatalf("resp.Body = %q; want %q", want, got)
+	}
+
+	// Flush twice to expose issue of exporter creating traces internally (#557)
+	exporter.Flush()
+	exporter.Flush()
+
+	for _, err := range exportErrors {
+		t.Error(err)
+	}
+}
